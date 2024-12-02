528509a337cd2b873f4cab016132a2accdea3fc2
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 16:27:56 +0530

    Update app component by wraping all routes with browserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+     <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/Register" element={<Register />}/>
+            <Route path="/dashboard" element={<Dashboard />}/>
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
