ca3f28946106376afa0a0dfce547b6442df2c023
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 15:26:09 +0530

    Add style to the label and input elements of username

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,11 @@
 function Login() {
   return (
     <>
-      <div>
-        <label>User name</label>
-        <input placeholder="enter username" />
+    <div style={{margin:50, gap:30}}>
+      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
+        <label style={{justifyContent:"flex-start"}}>User name</label>
+        <input style={{ height:30, width:400}}placeholder="enter username" />
+      </div>
       </div>
     </>
   );
