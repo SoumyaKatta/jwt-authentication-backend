d6edd9805f5be79deade23c8f0738736b04e8af5
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 15:43:40 +0530

    Create anchor element

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,50 @@
 function Login() {
   return (
     <>
-    <div style={{margin:50,display:'flex', flexDirection:"column",gap:20}}>
-      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
-        <label style={{justifyContent:"flex-start"}}>User name</label>
-        <input style={{ height:30, width:400, marginBottom:20}}placeholder="enter username" />
-      </div>
-      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
-        <label style={{justifyContent:"flex-start"}}>Password</label>
-        <input style={{ height:30, width:400}}placeholder="enter password" />
-      </div>
-      <button style={{ marginTop:20, height:30, width:100}}>Login</button>
+      <div
+        style={{
+          margin: 50,
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 20,
+          }}
+        >
+          <label style={{ justifyContent: "flex-start" }}>User name</label>
+          <input
+            style={{ height: 30, width: 400, marginBottom: 20 }}
+            placeholder="enter username"
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 20,
+          }}
+        >
+          <label style={{ justifyContent: "flex-start" }}>Password</label>
+          <input
+            style={{ height: 30, width: 400 }}
+            placeholder="enter password"
+          />
+        </div>
+        <button style={{ marginTop: 20, height: 30, width: 100 }}>Login</button>
+        <text>
+          don't have an account!please <a  href={'login'}>Register</a>
+        </text>
       </div>
     </>
   );
