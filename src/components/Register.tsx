6398b8da03a8830ae2f58b8f8afaabf4447ff196
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 16:32:50 +0530

    Update the register screen

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,23 +1,66 @@
-function Register(){
-    return(
-        <>
-        <div style={{margin:50,display:'flex', flexDirection:"column",gap:20}}>
-      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
-        <label style={{justifyContent:"flex-start"}}>User name</label>
-        <input style={{ height:30, width:400, marginBottom:20}}placeholder="enter username" />
+function Register() {
+  return (
+    <>
+      <div
+        style={{
+          margin: 50,
+          display: "flex",
+          flexDirection: "column",
+          gap: 20,
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 20,
+          }}
+        >
+          <label style={{ justifyContent: "flex-start" }}>User name</label>
+          <input
+            style={{ height: 30, width: 400, marginBottom: 20 }}
+            placeholder="enter username"
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 20,
+          }}
+        >
+          <label style={{ justifyContent: "flex-start" }}>Email</label>
+          <input style={{ height: 30, width: 400 }} placeholder="enter Email" />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "flex-start",
+            gap: 20,
+          }}
+        >
+          <label style={{ justifyContent: "flex-start" }}>Password</label>
+          <input
+            style={{ height: 30, width: 400 }}
+            placeholder="enter password"
+          />
+        </div>
+        <button style={{ marginTop: 20, height: 30, width: 100 }}>
+          Register
+        </button>
+        <text>
+          Already have an account!please <a href={"/"}>login</a>
+        </text>
       </div>
-      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
-        <label style={{justifyContent:"flex-start"}}>Email</label>
-        <input style={{ height:30, width:400}}placeholder="enter Email" />
-      </div>
-      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
-        <label style={{justifyContent:"flex-start"}}>Password</label>
-        <input style={{ height:30, width:400}}placeholder="enter password" />
-      </div>
-      <button style={{ marginTop:20, height:30, width:100}}>Register</button>
-      <text>Already have an account!please <a href={'Login'}>login</a></text>
-      </div>
-        </>
-    )
+    </>
+  );
 }
-export default Register
+export default Register;
