1f6118081b31c5bd8c11e2507e263c18770f5c6c
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 15:35:30 +0530

    Create the login button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,7 @@
 function Login() {
   return (
     <>
-    <div style={{margin:50}}>
+    <div style={{margin:50,display:'flex', flexDirection:"column",gap:20}}>
       <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
         <label style={{justifyContent:"flex-start"}}>User name</label>
         <input style={{ height:30, width:400, marginBottom:20}}placeholder="enter username" />
@@ -10,6 +10,7 @@ function Login() {
         <label style={{justifyContent:"flex-start"}}>Password</label>
         <input style={{ height:30, width:400}}placeholder="enter password" />
       </div>
+      <button style={{ marginTop:20, height:30, width:100}}>Login</button>
       </div>
     </>
   );
