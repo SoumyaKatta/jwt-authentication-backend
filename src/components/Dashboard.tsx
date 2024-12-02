a909e57c3f1bd0d06c913917ed45f7129411d212
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 15:48:20 +0530

    Create dashboard screen

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,10 @@
+function Dashboard(){
+    return(
+        <>
+        <div style={{margin:40, justifyContent:'center'}}>
+           <text>Hey!! Welcome to Dashboard</text>
+        </div>
+        </>
+    )
+}
+export default Dashboard;
