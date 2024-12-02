84c111f91585179a02c114c46923c1571b121e33
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 15:21:09 +0530

    Create label and input elements for username

---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <>
+      <div>
+        <label>User name</label>
+        <input placeholder="enter username" />
+      </div>
+    </>
+  );
+}
+export default Login;
