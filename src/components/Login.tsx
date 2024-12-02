da87576a2e62c677107cb5fc2c9604b123b58eca
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-02 15:28:46 +0530

    Create label and input elements for password field

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,10 +1,14 @@
 function Login() {
   return (
     <>
-    <div style={{margin:50, gap:30}}>
+    <div style={{margin:50}}>
       <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
         <label style={{justifyContent:"flex-start"}}>User name</label>
-        <input style={{ height:30, width:400}}placeholder="enter username" />
+        <input style={{ height:30, width:400, marginBottom:20}}placeholder="enter username" />
+      </div>
+      <div style={{display:'flex', flexDirection:"column", justifyContent:'flex-start', alignItems:"flex-start", gap:20}}>
+        <label style={{justifyContent:"flex-start"}}>Password</label>
+        <input style={{ height:30, width:400}}placeholder="enter password" />
       </div>
       </div>
     </>
