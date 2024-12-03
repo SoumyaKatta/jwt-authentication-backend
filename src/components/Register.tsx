36726eaed3fa0846bac3daa98299a1449d08d440
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-03 23:45:34 +0530

    Implement the handleRegister functionality

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,4 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleRegister = async (username: any, email: any, password: any) => {
+    try {
+      const res = await fetch(`http://localhost:4000/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+      console.log("heelelo", res.status);
+      const data = await res.json();
+      if (res.status == 201) {
+        navigate("/");
+      }
+    } catch (err) {
+      return err;
+    }
+  };
   return (
     <>
       <div
@@ -24,6 +50,10 @@ function Register() {
           <input
             style={{ height: 30, width: 400, marginBottom: 20 }}
             placeholder="enter username"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
           />
         </div>
         <div
@@ -36,7 +66,14 @@ function Register() {
           }}
         >
           <label style={{ justifyContent: "flex-start" }}>Email</label>
-          <input style={{ height: 30, width: 400 }} placeholder="enter Email" />
+          <input
+            style={{ height: 30, width: 400 }}
+            placeholder="enter Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
         </div>
         <div
           style={{
@@ -51,9 +88,16 @@ function Register() {
           <input
             style={{ height: 30, width: 400 }}
             placeholder="enter password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           />
         </div>
-        <button style={{ marginTop: 20, height: 30, width: 100 }}>
+        <button
+          style={{ marginTop: 20, height: 30, width: 100 }}
+          onClick={() => handleRegister(username, email, password)}
+        >
           Register
         </button>
         <text>
