5a3872476dde899fd838076e60eae8e114f38e90
SoumyaKatta <soumya.katta@everest.engineering>
2024-12-03 14:20:25 +0530

    Create a post route for register the user

--- server.ts
+++ server.ts
@@ -1,10 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const express = require('express');
 const app = express();
-const Users = require('./src/models/User')
+const User = require('./src/models/User')
 app.use(express.json())
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 app.get('/',(req:any,res:any)=>{
     res.json('loginn success')
+})
+app.post('/register',async(req:any,res:any)=>{
+    const {username, email, password} = req.body;
+    try{
+        console.log("usermodel was")
+        const user = await User.findOne({email:email})
+        if(user){
+         res.json("user already exists")
+      }
+      const hashpassword = await bcrypt.hash(password, 12)
+      const newUser = await User.create({
+        ...req.body,
+        password:hashpassword
+      })
+      const token = jwt.sign({_id:newUser._id},'secretkey123',{
+        expiresIn:'2M'
+      })
+      res.status(201).json({
+        status:"success",
+        message:"user registered",
+        token,
+      })
+    }
+    catch(err){
+      res.json(err)
+    }
+
 })
 app.listen(4000);
 export default app;
